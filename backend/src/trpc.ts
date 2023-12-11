825b046c849ae66091bf93d3c841bb7b715d2e97
Akash Singh <developer.akash8@gmail.com>
2023-12-11 19:51:55 +0530

    backend: setup env variables

--- backend/src/trpc.ts
+++ backend/src/trpc.ts
@@ -1,6 +1,8 @@
 import { initTRPC } from "@trpc/server";
+import { config } from "dotenv";
 
 const t = initTRPC.create();
+config();
 
 export const router = t.router;
 export const publicProcedure = t.procedure;
