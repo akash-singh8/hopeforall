845095534e0dbf16089e81d58047350a7fa168c9
Akash Singh <developer.akash8@gmail.com>
2023-12-11 12:49:15 +0530

    frontend: setup trpc client

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,15 +4,36 @@ import Footer from "./components/footer";
 import Hope4All from "./components/Hope4All";
 import Approach from "./components/Approach";
 
+import { useState } from "react";
+import type { AppRouter } from "../../backend/src/index.ts";
+import { createTRPCReact } from "@trpc/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
+
 function App() {
+  const trpc = createTRPCReact<AppRouter>();
+
+  const [queryClient] = useState(() => new QueryClient());
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: "http://localhost:3000",
+        }),
+      ],
+    })
+  );
+
   return (
-    <>
-      <Navbar />
-      <Home />
-      <Hope4All />
-      <Approach />
-      <Footer />
-    </>
+    <trpc.Provider client={trpcClient} queryClient={queryClient}>
+      <QueryClientProvider client={queryClient}>
+        <Navbar />
+        <Home />
+        <Hope4All />
+        <Approach />
+        <Footer />
+      </QueryClientProvider>
+    </trpc.Provider>
   );
 }
 
