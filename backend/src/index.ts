f4b2beb745eb662a989ffc936f080f9d71067534
Akash Singh <developer.akash8@gmail.com>
2023-12-11 21:07:33 +0530

    procedure: added for customer contact

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,18 @@
 import { router, publicProcedure } from "./trpc";
 import { createHTTPServer } from "@trpc/server/adapters/standalone";
 import cors from "cors";
+import sendMail from "./controller/sendMail";
+import contactSchema from "./validation/contact";
 
 const appRouter = router({
   hello: publicProcedure.query(() => {
     return "Hello Universe";
+  }),
+
+  contact: publicProcedure.input(contactSchema).mutation(({ input }) => {
+    sendMail(process.env.CEO_EMAIL!, input);
+
+    return { message: "Successfully send the mail" };
   }),
 });
 
