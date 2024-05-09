8c42e931574bbb4d8a922ee84b27849b5959c5d5
Akash Singh <developer.akash8@gmail.com>
2024-05-09 22:09:38 +0530

    fix: remove trpc

--- frontend/src/components/Contact.tsx
+++ frontend/src/components/Contact.tsx
@@ -1,22 +1,13 @@
 import style from "../styles/Contact.module.css";
 import crowdfunding from "../assets/crowdfunding.png";
-import { trpc } from "../utils/trpc";
 
 const Contact = () => {
-  const contact = trpc.contact.useMutation();
-
   const handleContact = (e: any) => {
     e.preventDefault();
 
     const name = document.getElementById("name") as HTMLInputElement;
     const email = document.getElementById("email") as HTMLInputElement;
     const message = document.getElementById("message") as HTMLInputElement;
-
-    contact.mutate({
-      name: name.value,
-      email: email.value,
-      message: message.value,
-    });
 
     alert("Successfully send the message!");
     name.value = "";
