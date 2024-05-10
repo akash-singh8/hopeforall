be47606e886449ef26f827ea1f7bc3c5fab10351
iamnotBugs <balrajdhakar2002@gmail.com>
2024-05-10 13:06:21 +0530

    fix: Donor Page

--- frontend/src/components/DonorPgae.tsx
+++ frontend/src/components/DonorPgae.tsx
@@ -1,173 +1,99 @@
 import "../styles/DonorPage.css";
 import bloodDonor from "../assets/blood.jpeg";
-import donor1 from "../assets/donor1.png";
 import donor2 from "../assets/donor2.jpeg";
-import donor3 from "../assets/donor3.jpeg";
-import donor4 from "../assets/donor4.jpg";
-import donor5 from "../assets/donor5.jpeg";
-import donor6 from "../assets/donor6.jpeg";
+import donor4 from "../assets/donor3.jpeg";
+// import donor4 from "../assets/donor4.jpg";
+// import donor4 from "../assets/donor5.jpeg";
+import donor3 from "../assets/donor6.jpeg";
 
-const DonorPgae = () => {
+const DonorPage = () => {
   return (
     <>
-      <div className="donoorpage">
+      <div className="donorpage">
         <div className="donorclass">
           <h1 className="donortitle">Donor Page</h1>
-
           <div className="donorcards">
-            <div className="card">
-              <img src={bloodDonor} alt="Blood Donor" className="bloodimage" />
-              <div className="descrip">
-                <p className="urgent">
-                  Urgent Medical Fundraiser: Help Save Sivasanskari's Life!
-                </p>
-                <div className="profile">
-                  <img
-                    src={donor1}
-                    alt="Blood Donor"
-                    className="donorPropfileimg"
-                  />
-                  <p className="donorname">by Sabyasachi Chatterjee</p>
-                </div>
-                <div className="raised">
-                  <p className="raisedamount">₹3,22,990 raised</p>
-                  <div className="linetime"></div>
-                  <p className="donorraised">last donation 7 minutes ago</p>
-                </div>
-
-                <div className="time">
-                  <p className="timeleft">16 Days Left</p>
-                  <p className="timeleft">93 Supporters</p>
-                </div>
-              </div>
-            </div>
             <div className="card">
               <img src={donor2} alt="Blood Donor" className="bloodimage" />
               <div className="descrip">
                 <p className="urgent">
-                  Urgent Medical Fundraiser: Help Save Sivasanskari's Life!
+                  Urgent Medical Fundraiser: Help Save John's Life!
                 </p>
                 <div className="profile">
-                  <img
-                    src={donor1}
-                    alt="Blood Donor"
-                    className="donorPropfileimg"
-                  />
-                  <p className="donorname">by Sabyasachi Chatterjee</p>
+                  <p className="donorname">by Emily Johnson</p>
                 </div>
                 <div className="raised">
-                  <p className="raisedamount">₹3,22,990 raised</p>
+                  <p className="raisedamount">₹5,00,000 raised</p>
                   <div className="linetime"></div>
-                  <p className="donorraised">last donation 7 minutes ago</p>
+                  <p className="donorraised">last donation 15 minutes ago</p>
                 </div>
 
                 <div className="time">
-                  <p className="timeleft">16 Days Left</p>
-                  <p className="timeleft">93 Supporters</p>
+                  <p className="timeleft">10 Days Left</p>
+                  <p className="timeleft">120 Supporters</p>
                 </div>
               </div>
             </div>
             <div className="card">
-              <img src={donor3} alt="Blood Donor" className="bloodimage" />
+              <img src={bloodDonor} alt="Blood Donor" className="bloodimage" />
               <div className="descrip">
                 <p className="urgent">
-                  Urgent Medical Fundraiser: Help Save Sivasanskari's Life!
+                  Urgent Medical Fundraiser: Help Save Mary's Life!
                 </p>
                 <div className="profile">
-                  <img
-                    src={donor1}
-                    alt="Blood Donor"
-                    className="donorPropfileimg"
-                  />
-                  <p className="donorname">by Sabyasachi Chatterjee</p>
+                  <p className="donorname">by Michael Smith</p>
                 </div>
                 <div className="raised">
-                  <p className="raisedamount">₹3,22,990 raised</p>
+                  <p className="raisedamount">₹3,75,000 raised</p>
                   <div className="linetime"></div>
-                  <p className="donorraised">last donation 7 minutes ago</p>
+                  <p className="donorraised">last donation 20 minutes ago</p>
                 </div>
 
                 <div className="time">
-                  <p className="timeleft">16 Days Left</p>
-                  <p className="timeleft">93 Supporters</p>
+                  <p className="timeleft">7 Days Left</p>
+                  <p className="timeleft">80 Supporters</p>
                 </div>
               </div>
             </div>
             <div className="card">
-              <img src={donor4} alt="Blood Donor" className="bloodimage" />
+                <img src={donor3} alt="Blood Donor" className="bloodimage" />
               <div className="descrip">
                 <p className="urgent">
-                  Urgent Medical Fundraiser: Help Save Sivasanskari's Life!
+                  Urgent Medical Fundraiser: Help Save David's Life!
                 </p>
                 <div className="profile">
-                  <img
-                    src={donor1}
-                    alt="Blood Donor"
-                    className="donorPropfileimg"
-                  />
-                  <p className="donorname">by Sabyasachi Chatterjee</p>
+                  <p className="donorname">by Sarah Brown</p>
                 </div>
                 <div className="raised">
-                  <p className="raisedamount">₹3,22,990 raised</p>
+                  <p className="raisedamount">₹2,10,000 raised</p>
                   <div className="linetime"></div>
-                  <p className="donorraised">last donation 7 minutes ago</p>
+                  <p className="donorraised">last donation 30 minutes ago</p>
                 </div>
 
                 <div className="time">
-                  <p className="timeleft">16 Days Left</p>
-                  <p className="timeleft">93 Supporters</p>
+                  <p className="timeleft">5 Days Left</p>
+                  <p className="timeleft">60 Supporters</p>
                 </div>
               </div>
             </div>
             <div className="card">
-              <img src={donor5} alt="Blood Donor" className="bloodimage" />
+                <img src={donor4} alt="Blood Donor" className="bloodimage" />
               <div className="descrip">
                 <p className="urgent">
-                  Urgent Medical Fundraiser: Help Save Sivasanskari's Life!
+                  Urgent Medical Fundraiser: Help Save Emma's Life!
                 </p>
                 <div className="profile">
-                  <img
-                    src={donor1}
-                    alt="Blood Donor"
-                    className="donorPropfileimg"
-                  />
-                  <p className="donorname">by Sabyasachi Chatterjee</p>
+                  <p className="donorname">by James Wilson</p>
                 </div>
                 <div className="raised">
-                  <p className="raisedamount">₹3,22,990 raised</p>
+                  <p className="raisedamount">₹1,50,000 raised</p>
                   <div className="linetime"></div>
-                  <p className="donorraised">last donation 7 minutes ago</p>
+                  <p className="donorraised">last donation 40 minutes ago</p>
                 </div>
 
                 <div className="time">
-                  <p className="timeleft">16 Days Left</p>
-                  <p className="timeleft">93 Supporters</p>
-                </div>
-              </div>
-            </div>
-            <div className="card">
-              <img src={donor6} alt="Blood Donor" className="bloodimage" />
-              <div className="descrip">
-                <p className="urgent">
-                  Urgent Medical Fundraiser: Help Save Sivasanskari's Life!
-                </p>
-                <div className="profile">
-                  <img
-                    src={donor1}
-                    alt="Blood Donor"
-                    className="donorPropfileimg"
-                  />
-                  <p className="donorname">by Sabyasachi Chatterjee</p>
-                </div>
-                <div className="raised">
-                  <p className="raisedamount">₹3,22,990 raised</p>
-                  <div className="linetime"></div>
-                  <p className="donorraised">last donation 7 minutes ago</p>
-                </div>
-
-                <div className="time">
-                  <p className="timeleft">16 Days Left</p>
-                  <p className="timeleft">93 Supporters</p>
+                  <p className="timeleft">3 Days Left</p>
+                  <p className="timeleft">40 Supporters</p>
                 </div>
               </div>
             </div>
@@ -178,4 +104,4 @@ const DonorPgae = () => {
   );
 };
 
-export default DonorPgae;
+export default DonorPage;
